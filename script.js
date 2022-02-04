cce0951617b8037a06e3cedd7a92961b4208c4be
humbertodutra <humberto.dutra@hotmail.com>
2022-02-03 23:03:32 -0300

    bumbaaaaa

--- script.js
+++ script.js
@@ -1,81 +1,87 @@
+const paletaDeCores = document.querySelectorAll('.color');
+console.log(paletaDeCores[0]);
+let colorSelected = 'black';
+const quadroDeCores = document.getElementById('pixel-board');
+const btnClean = document.getElementById('clear-board');
 
-let selectedColor = "black";
+btnClean.addEventListener('click', () => {
+  const pixel = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixel.length; index += 1) {
+    const element = pixel[index];
+    element.style.backgroundColor = 'white';
+  }
+  console.log(pixel.length);
+});
 
-const colors = ['black' , 'green', 'gold', 'gray']
+quadroDeCores.addEventListener('click', (e) => {
+  e.target.style.backgroundColor = colorSelected;
+});
 
-
-let o = document.getElementsByClassName("color");
-
-for (let i = 0 ; i < colors.length ; i += 1){
-
-
-  
-    
-        o[i].style.backgroundColor = colors[i];
-        o[i].addEventListener('click', selceionarCor);  
-
-
+function selectColor(e) {
+  const g = e.target;
+  document.getElementsByClassName('selected')[0].classList.remove('selected');
+  g.classList.add('selected');
+  colorSelected = e.target.style.backgroundColor;
+  console.log(colorSelected);
 }
 
+paletaDeCores[0].addEventListener('click', selectColor);
+paletaDeCores[1].addEventListener('click', selectColor);
+paletaDeCores[2].addEventListener('click', selectColor);
+paletaDeCores[3].addEventListener('click', selectColor);
 
-
-function selceionarCor (element) {   
-
-  let g = element.target
-
-  
-
-  document.getElementsByClassName('selected')[0].classList.remove('selected')
-  g.classList.add('selected')
-
-
-  selectedColor = g.style.backgroundColor;
-  
-  
+function gerarCor() {
+  const r = Math.random() * 255;
+  const g = Math.random() * 255;
+  const b = Math.random() * 255;
+  const rgba = `rgb(${r}, ${g}, ${b})`;
+  return rgba;
 }
 
-
-z = document.getElementsByClassName('pixel');
-
-for (let i = 0 ; i < z.length ; i += 1){
-
-
-    
-    
-    
-     z[i].addEventListener('click', corQuadro);  
-
-
+function loadColors() {
+  paletaDeCores[0].style.backgroundColor = 'black';
+  paletaDeCores[1].style.backgroundColor = gerarCor();
+  paletaDeCores[2].style.backgroundColor = gerarCor();
+  paletaDeCores[3].style.backgroundColor = gerarCor();
 }
 
-function corQuadro (element) {
+const input = document.getElementById('board-size');
+const vqv = document.getElementById('generate-board');
 
-    let h = element.target
-    h.style.backgroundColor = selectedColor
- 
-
+function deleteBoard() {
+  const pixels = document.querySelectorAll('.pixel');
+  if (pixels.length > 0) {
+    for (let i = 0; i < pixels.length; i += 1) {
+      pixels[i].remove();
+    }
+  }
 }
 
-
-function limparQuadro () {
-
-        for (let x = 0 ; x < z.length ; x += 1) {
-            
-            z[x].style.backgroundColor = 'white';
-
-
-        }
-
-        
+function verify(value) {
+  if (value <= 5) return 5;
+  if (value >= 50) return 50;
+  return value;
+}
+function newBoard() {
+  const { value } = input;
+  const newValue = verify(value);
+  if (value === '') {
+    alert('Board inválido!');
+  } else {
+    quadroDeCores.style = `grid-template-columns: repeat(${newValue}, 40px);`;
+    for (let index = 0; index < (newValue * newValue); index += 1) {
+      const creating = document.createElement('div');
+      creating.className = 'pixel';
+      quadroDeCores.appendChild(creating);
+    }
+  }
 }
 
+vqv.addEventListener('click', () => {
+  deleteBoard();
+  newBoard();
+});
 
-    document.getElementById("clear-board").addEventListener('click', limparQuadro)
-
-
-
-
-
-
-
-
+window.onload = function onload() {
+  loadColors();
+};
